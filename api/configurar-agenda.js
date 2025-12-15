85f0ebd61d77e71447680008d275c023d7362e54
jrgarcia71 <55211995+jrgarcia71@users.noreply.github.com>
2025-12-15 18:46:15 -0500

    Update configurar-agenda.js

--- api/configurar-agenda.js
+++ api/configurar-agenda.js
@@ -1,11 +1,12 @@
-import { createClient } from "@supabase/supabase-js";
+
+const { createClient } = require("@supabase/supabase-js");
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_ANON_KEY
 );
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Método no permitido" });
   }
@@ -50,4 +51,4 @@ export default async function handler(req, res) {
     console.error(err);
     return res.status(500).json({ error: "Error inesperado" });
   }
-}
+};
